c1c60bb8f9941f25a4458affcda4095dc08ac23e
hce <vmalikv87@gmail.com>
2021-10-01 22:21:52 +0530

    resume-updated

--- src/home.jsx
+++ src/home.jsx
@@ -148,7 +148,10 @@ function home(props) {
                     </div>
                   </div>
                   <div className="download-btn mt-5 text-center ">
-                    <a href="https://drive.google.com/u/0/uc?id=1AxoHwcIPv-HnE_sdoe51gFylmb48_3Cs&export=download">
+                    <a
+                      href="
+https://drive.google.com/u/0/uc?id=1epaCyL4F9GfnPR-p-sZu8lILgX0YXfiV&export=download"
+                    >
                       <Button
                         variant="outline-light"
                         // style={{ minWidth: "350px" }}
